d531cf7642fea818f759a863152cd97ba30729f4
Jp_zs <joaopedrosartori2016@gmail.com>
2025-05-06 14:36:57 -0300

    Atualização 05/05/2025

--- aulas/aula14042025_poo/GerenciadorDeCompromissos/GerenciadorDeCompromissos/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ aulas/aula14042025_poo/GerenciadorDeCompromissos/GerenciadorDeCompromissos/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc2403912636d8f2a84b80dca7f85f09d2a86ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d9c9d81b84a18c401606f8dbd3bead00a86c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
